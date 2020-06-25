dd8d51357c69b4d7675dff59d55b5607237e0d77
Vincent Perrier <perrier.vincent0210@gmail.com>
2020-06-24 22:30:01 -0400

    Created file for the implementation of input socket module

---
+++ src/main/java/com/vincentperrier/softOscilloscope/inputSocket.java
@@ -0,0 +1,28 @@
+package com.vincentperrier.softOscilloscope;
+
+public class inputSocket implements input {
+    @Override
+    public void loopRead() {
+
+    }
+
+    @Override
+    public void setName(String name) {
+
+    }
+
+    @Override
+    public void setBaudRate(int rate) {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public int getBaudRate() {
+        return 0;
+    }
+}
